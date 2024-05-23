2f35166b5eadd615942051ff870578b1b32876b3
MrHydralisk <113328125+MrHydralisk@users.noreply.github.com>
2024-05-23 17:43:22 +0300

    MeatGrinder corpse consumtion

--- Source/Anomalies Expected/Comp/Comp_MeatGrinder.cs
+++ Source/Anomalies Expected/Comp/Comp_MeatGrinder.cs
@@ -20,6 +20,58 @@ namespace AnomaliesExpected
         protected CompStudiable Studiable => studiableCached ?? (studiableCached = parent.TryGetComp<CompStudiable>());
         private CompStudiable studiableCached;
 
+        public bool isActive = true;
+
+        public override void CompTickRare()
+        {
+            base.CompTickRare();
+            if (isActive)
+            {
+                List<Thing> consumables = ConsumtionCells.SelectMany((IntVec3 iv3) => parent.Map.thingGrid.ThingsListAtFast(iv3)).ToList();
+                bool isConsumed = false;
+                if (consumables.Count > 0)
+                {
+                    foreach (Thing consumable in consumables)
+                    {
+                        if (Butcher(consumable))
+                        {
+                            isConsumed = true;
+                            break;
+                        }
+                    }
+                }
+                if (!isConsumed)
+                {
+                    //isActive = false;
+                }
+            }
+        }
+
+        public bool Butcher(Thing thing)
+        {
+            if (thing is Corpse corpse)
+            {
+                IEnumerable<Thing> products = corpse.ButcherProducts(parent.Map?.mapPawns?.FreeColonists?.RandomElement(), 1);
+                foreach (Thing product in products)
+                {
+                    GenPlace.TryPlaceThing(product, parent.Position, parent.Map, ThingPlaceMode.Near, null, delegate (IntVec3 newLoc)
+                    {
+                        foreach (Thing item in parent.Map.thingGrid.ThingsListAtFast(newLoc))
+                        {
+                            if (item == parent)
+                            {
+                                return false;
+                            }
+                        }
+                        return true;
+                    });
+                }
+                corpse.Destroy();
+                return true;
+            }
+            return false;
+        }
+
         private void CreateCorpseStockpile()
         {
             Zone_Stockpile stockpile = new Zone_Stockpile(StorageSettingsPreset.CorpseStockpile, parent.Map.zoneManager);
