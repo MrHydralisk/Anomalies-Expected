1a18eb8868c8f3086b0c2566baed2572559fc6a5
MrHydralisk <113328125+MrHydralisk@users.noreply.github.com>
2024-05-30 20:28:42 +0300

    BakingPies Texture and Basic xml and Comp

---
+++ Source/Anomalies Expected/Comp/Comp_BakingPies.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace AnomaliesExpected
+{
+    public class Comp_BakingPies : ThingComp
+    {
+        public CompProperties_BakingPies Props => (CompProperties_BakingPies)props;
+
+        private List<Thing> piePieces = new List<Thing>();
+
+        private int piePiecesExisting => piePieces.Count((Thing t) => !(t?.Destroyed ?? true));
+
+        private int TickSpawn;
+
+        protected CompAEStudyUnlocks StudyUnlocks => studyUnlocksCached ?? (studyUnlocksCached = parent.TryGetComp<CompAEStudyUnlocks>());
+        private CompAEStudyUnlocks studyUnlocksCached;
+
+        public override void PostSpawnSetup(bool respawningAfterLoad)
+        {
+            base.PostSpawnSetup(respawningAfterLoad);
+            if (!respawningAfterLoad)
+            {
+                TickSpawn = Find.TickManager.TicksGame + Props.tickPerSpawn;
+            }
+        }
+
+        public override void CompTickLong()
+        {
+            base.CompTickLong();
+            if (Find.TickManager.TicksGame >= TickSpawn)
+            {
+                SpawnPiePiece();
+                TickSpawn = Find.TickManager.TicksGame + Props.tickPerSpawn;
+            }
+        }
+
+        public void SpawnPiePiece()
+        {
+            if (!piePieces.Contains(parent))
+            {
+                piePieces.Insert(0, parent);
+            }
+            int amount = piePiecesExisting;
+            int l = 0;
+            for (int i = 0; i < piePieces.Count; i++)
+            {
+                Thing piePiece = piePieces.ElementAtOrDefault(i);
+                if (piePiece != null && !piePiece.Destroyed)
+                {
+                    if (piePiece.Spawned && i >= l)
+                    {
+                        IntVec3 pos = piePiece.Position;
+                        Map map = piePiece.Map ?? parent.Map;
+                        for (int j = 0; j < amount; j++)
+                        {
+                            Thing piece = ThingMaker.MakeThing(Props.piePiece);
+                            piePieces.Add(piece);
+                            bool isPlaced = GenPlace.TryPlaceThing(piece, pos, map, ThingPlaceMode.Near, null);
+                            if (isPlaced)
+                            {
+                                amount -= 1;
+                                if (amount <= 0)
+                                {
+                                    break;
+                                }
+                            }
+                            else
+                            {
+                                l = i + Mathf.Min(8, amount / 5000);
+                                break;
+                            }
+                        }
+                        if (amount <= 0)
+                        {
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+                    piePieces.RemoveAt(i);
+                    i--;
+                }
+            }
+        }
+
+        public override IEnumerable<Gizmo> CompGetGizmosExtra()
+        {
+            foreach (Gizmo gizmo in base.CompGetGizmosExtra())
+            {
+                yield return gizmo;
+            }
+            if (DebugSettings.ShowDevGizmos)
+            {
+                yield return new Command_Action
+                {
+                    action = delegate
+                    {
+                        List<FloatMenuOption> floatMenuOptions = new List<FloatMenuOption>();
+                        floatMenuOptions.Add(new FloatMenuOption("Increase by 3d", delegate
+                        {
+                            TickSpawn += 180000;
+                        }));
+                        floatMenuOptions.Add(new FloatMenuOption("Increase by 1d", delegate
+                        {
+                            TickSpawn += 60000;
+                        }));
+                        floatMenuOptions.Add(new FloatMenuOption("Increase by 1h", delegate
+                        {
+                            TickSpawn += 2500;
+                        }));
+                        floatMenuOptions.Add(new FloatMenuOption("Decrease by 1h", delegate
+                        {
+                            TickSpawn -= 2500;
+                        }));
+                        floatMenuOptions.Add(new FloatMenuOption("Decrease by 1d", delegate
+                        {
+                            TickSpawn -= 60000;
+                        }));
+                        floatMenuOptions.Add(new FloatMenuOption("Decrease by 3d", delegate
+                        {
+                            TickSpawn -= 180000;
+                        }));
+                        Find.WindowStack.Add(new FloatMenu(floatMenuOptions));
+                    },
+                    defaultLabel = "Dev: Change Spawn",
+                    defaultDesc = $"Change timer till spawn piece of pie: {(TickSpawn - Find.TickManager.TicksGame).ToStringTicksToDays()}"
+                };
+                yield return new Command_Action
+                {
+                    action = delegate
+                    {
+                        SpawnPiePiece();
+                    },
+                    defaultLabel = "Dev: Spawn",
+                    defaultDesc = $"Spawn piece of pie: {(TickSpawn - Find.TickManager.TicksGame).ToStringTicksToDays()}"
+                };
+            }
+        }
+
+        public override void PostExposeData()
+        {
+            base.PostExposeData();
+            Scribe_Collections.Look(ref piePieces, "piePieces", LookMode.Value);
+            Scribe_Values.Look(ref TickSpawn, "TickSpawn", Find.TickManager.TicksGame + Props.tickPerSpawn);
+        }
+
+
+        public override string CompInspectStringExtra()
+        {
+            List<string> inspectStrings = new List<string>();
+            int study = StudyUnlocks?.NextIndex ?? 4;
+            if (study > 1)
+            {
+                inspectStrings.Add("AnomaliesExpected.BakingPies.Amount".Translate(piePiecesExisting).RawText);
+            }
+            return String.Join("\n", inspectStrings);
+        }
+    }
+}
---
+++ Source/Anomalies Expected/CompProperties/CompProperties_BakingPies.cs
@@ -0,0 +1,16 @@
+﻿using RimWorld;
+using System.Collections.Generic;
+using Verse;
+
+namespace AnomaliesExpected
+{
+    public class CompProperties_BakingPies : CompProperties
+    {
+        public int tickPerSpawn = 60000;
+        public ThingDef piePiece;
+        public CompProperties_BakingPies()
+        {
+            compClass = typeof(Comp_BakingPies);
+        }
+    }
+}
