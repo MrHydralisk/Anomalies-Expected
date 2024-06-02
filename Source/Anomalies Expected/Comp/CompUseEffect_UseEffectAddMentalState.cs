62d7f58c2a51a82e1db76750dc2066838ac7ec89
MrHydralisk <113328125+MrHydralisk@users.noreply.github.com>
2024-06-02 17:18:08 +0300

    Entity, Research, Incident, Serum for BakingPies

---
+++ Source/Anomalies Expected/Comp/CompUseEffect_UseEffectAddMentalState.cs
@@ -0,0 +1,15 @@
+﻿using RimWorld;
+using Verse;
+
+namespace AnomaliesExpected
+{
+    public class CompUseEffect_UseEffectAddMentalState : CompUseEffect
+    {
+        public CompProperties_UseEffectAddMentalState Props => (CompProperties_UseEffectAddMentalState)props;
+
+        public override void DoEffect(Pawn user)
+        {
+            user.mindState.mentalStateHandler.TryStartMentalState(Props.mentalStateDef, forced: true);
+        }
+    }
+}
---
+++ Source/Anomalies Expected/CompProperties/CompProperties_UseEffectAddMentalState.cs
@@ -0,0 +1,15 @@
+﻿using RimWorld;
+using Verse;
+
+namespace AnomaliesExpected
+{
+    public class CompProperties_UseEffectAddMentalState : CompProperties_UseEffect
+    {
+        public MentalStateDef mentalStateDef;
+
+        public CompProperties_UseEffectAddMentalState()
+        {
+            compClass = typeof(CompUseEffect_UseEffectAddMentalState);
+        }
+    }
+}
