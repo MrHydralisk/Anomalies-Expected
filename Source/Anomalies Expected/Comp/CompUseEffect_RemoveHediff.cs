83ab5b2f777bbc4e8e6cbcb68860cb01ec50c78f
MrHydralisk <113328125+MrHydralisk@users.noreply.github.com>
2024-07-02 02:22:37 +0300

    Additional HediffRemoval effect of food serum and desc fix

---
+++ Source/Anomalies Expected/Comp/CompUseEffect_RemoveHediff.cs
@@ -0,0 +1,24 @@
+﻿using RimWorld;
+using Verse;
+
+namespace AnomaliesExpected
+{
+    public class CompUseEffect_RemoveHediff : CompUseEffect
+    {
+        public CompProperties_UseEffecRemoveHediff Props => (CompProperties_UseEffecRemoveHediff)props;
+
+        public override void DoEffect(Pawn user)
+        {
+            HealthUtility.AdjustSeverity(user, Props.hediffDef, -Props.severity);
+        }
+
+        public override AcceptanceReport CanBeUsedBy(Pawn p)
+        {
+            if (Props.requireHediffToUse && !p.health.hediffSet.HasHediff(Props.hediffDef))
+            {
+                return "MissingMaterials".Translate(Props.hediffDef.label);
+            }
+            return true;
+        }
+    }
+}
---
+++ Source/Anomalies Expected/CompProperties/CompProperties_UseEffecRemoveHediff.cs
@@ -0,0 +1,16 @@
+﻿using RimWorld;
+
+namespace AnomaliesExpected
+{
+    public class CompProperties_UseEffecRemoveHediff : CompProperties_UseEffectAddHediff
+    {
+        public float severity = 0.5f;
+
+        public bool requireHediffToUse;
+
+        public CompProperties_UseEffecRemoveHediff()
+        {
+            compClass = typeof(CompUseEffect_RemoveHediff);
+        }
+    }
+}
