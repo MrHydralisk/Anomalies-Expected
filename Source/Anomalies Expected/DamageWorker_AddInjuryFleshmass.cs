872669d05c50f03f23c312607c5e416cd05499ee
MrHydralisk <113328125+MrHydralisk@users.noreply.github.com>
2024-11-14 18:02:55 +0300

    Lake Fleshmass Spitter no longer deal dmg to Fleshmass

---
+++ Source/Anomalies Expected/DamageWorker_AddInjuryFleshmass.cs
@@ -0,0 +1,22 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace AnomaliesExpected
+{
+    public class DamageWorker_AddInjuryFleshmass : DamageWorker_AddInjury
+    {
+        public override DamageResult Apply(DamageInfo dinfo, Thing thing)
+        {
+            if (thing.Faction == Faction.OfEntities)
+            {
+                return new DamageResult();
+            }
+            return base.Apply(dinfo, thing);
+        }
+    }
+}
