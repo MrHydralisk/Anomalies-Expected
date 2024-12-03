6fb94349509ed3e455d8e9db018176ec6148b120
MrHydralisk <113328125+MrHydralisk@users.noreply.github.com>
2024-12-03 05:49:57 +0300

    Study Notes research projects now hidded till study note

---
+++ Source/Anomalies Expected/HarmonyPatches.cs
@@ -0,0 +1,30 @@
+﻿using HarmonyLib;
+using System;
+using Verse;
+
+namespace AnomaliesExpected
+{
+    [StaticConstructorOnStartup]
+    public class HarmonyPatches
+    {
+        private static readonly Type patchType;
+
+        static HarmonyPatches()
+        {
+            patchType = typeof(HarmonyPatches);
+            Harmony val = new Harmony("rimworld.mrhydralisk.AnomaliesExpectedPatch");
+
+            val.Patch(AccessTools.Property(typeof(ResearchProjectDef), "IsHidden").GetGetMethod(), prefix: new HarmonyMethod(patchType, "RPD_IsHidden_Prefix"));
+        }
+
+        public static bool RPD_IsHidden_Prefix(ref bool __result, ResearchProjectDef __instance)
+        {
+            if (!__instance.IsFinished && ModsConfig.AnomalyActive)
+            {
+                __result = Find.EntityCodex.Hidden(__instance) || !__instance.AnalyzedThingsRequirementsMet;
+                return false;
+            }
+            return true;
+        }
+    }
+}
