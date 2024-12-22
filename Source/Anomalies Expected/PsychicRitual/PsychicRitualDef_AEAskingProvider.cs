95ea8ca0e5380345f49954ca06f434233ef7d296
MrHydralisk <113328125+MrHydralisk@users.noreply.github.com>
2024-12-22 23:46:01 +0300

    Initial addition of ProviderScripture anomaly Moved PsychicRitual cs to the folder

---
+++ Source/Anomalies Expected/PsychicRitual/PsychicRitualDef_AEAskingProvider.cs
@@ -0,0 +1,30 @@
+﻿using RimWorld;
+using System.Collections.Generic;
+using System.Linq;
+using Verse;
+using Verse.AI.Group;
+
+namespace AnomaliesExpected
+{
+    public class PsychicRitualDef_AEAskingProvider : PsychicRitualDef_InvocationCircle
+    {
+        //public override List<PsychicRitualToil> CreateToils(PsychicRitual psychicRitual, PsychicRitualGraph graph)
+        //{
+        //    List<PsychicRitualToil> list = base.CreateToils(psychicRitual, graph);
+        //    list.Add(new PsychicRitualToil_AESummonBloodLake(InvokerRole));
+        //    return list;
+        //}
+
+        //public override IEnumerable<string> BlockingIssues(PsychicRitualRoleAssignments assignments, Map map)
+        //{
+        //    foreach (string item in base.BlockingIssues(assignments, map))
+        //    {
+        //        yield return item;
+        //    }
+        //    if (map.listerThings.AllThings.Any((Thing t) => t.def == ThingDefOfLocal.AE_BloodLake || t.def == ThingDefOfLocal.AE_BloodLakeSpawner))
+        //    {
+        //        yield return "AnomaliesExpected.BloodLake.AlreadyExists".Translate();
+        //    }
+        //}
+    }
+}
