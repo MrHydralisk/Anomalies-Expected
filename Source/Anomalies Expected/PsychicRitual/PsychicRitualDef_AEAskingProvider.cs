ad14e6cd30f70f72f6e3acaeecda2f2abae48ac3
MrHydralisk <113328125+MrHydralisk@users.noreply.github.com>
2024-12-24 00:40:07 +0300

    AskingProvider now create output resource based on input and scales from input Wealth, Research and ritual Quality Research AksingProvider max knowledge increased Fixed ProviderScripture study notes error on load

--- Source/Anomalies Expected/PsychicRitual/PsychicRitualDef_AEAskingProvider.cs
+++ Source/Anomalies Expected/PsychicRitual/PsychicRitualDef_AEAskingProvider.cs
@@ -1,6 +1,7 @@
 ﻿using RimWorld;
 using System.Collections.Generic;
 using System.Linq;
+using UnityEngine;
 using Verse;
 using Verse.AI.Group;
 
@@ -9,12 +10,26 @@ namespace AnomaliesExpected
     public class PsychicRitualDef_AEAskingProvider : PsychicRitualDef_InvocationCircle
     {
         public ThingDef ProviderBoxDef;
+        public float MaxWealth;
+        public ResearchProjectDef researchProjectDef;
+        public float maxResearchMult;
+        public List<Vector2> multFromQuality = new List<Vector2>();
         private Building_Storage ProviderBox;
+
+
+        private static Dictionary<PsychicRitualRoleDef, List<IntVec3>> tmpParticipants = new Dictionary<PsychicRitualRoleDef, List<IntVec3>>(8);
 
         public override List<PsychicRitualToil> CreateToils(PsychicRitual psychicRitual, PsychicRitualGraph graph)
         {
             List<PsychicRitualToil> list = base.CreateToils(psychicRitual, graph);
-            list.Add(new PsychicRitualToil_AEAskingProvider(InvokerRole) { ProviderBox = ProviderBox});
+            IReadOnlyDictionary<PsychicRitualRoleDef, List<IntVec3>> readOnlyDictionary = GenerateRolePositions(psychicRitual.assignments);
+            PsychicRitualToil_InvokeHorax psychicRitualToil_InvokeHorax = list.FirstOrDefault((PsychicRitualToil prt) => prt is PsychicRitualToil_InvokeHorax) as PsychicRitualToil_InvokeHorax;
+            list.Replace(psychicRitualToil_InvokeHorax, new PsychicRitualToil_AEAskingProvider(InvokerRole, readOnlyDictionary.TryGetValue(InvokerRole), TargetRole, readOnlyDictionary.TryGetValue(TargetRole), ChanterRole, readOnlyDictionary.TryGetValue(ChanterRole), DefenderRole, readOnlyDictionary.TryGetValue(DefenderRole), RequiredOffering)
+            {
+                hoursUntilHoraxEffect = psychicRitualToil_InvokeHorax.hoursUntilHoraxEffect,
+                hoursUntilOutcome = psychicRitualToil_InvokeHorax.hoursUntilOutcome,
+                ProviderBox = ProviderBox
+            });
             return list;
         }
 
@@ -35,5 +50,95 @@ namespace AnomaliesExpected
                 yield return "AnomaliesExpected.ProviderScripture.Ritual.BoxEmpty".Translate();
             }
         }
+
+        public override TaggedString OutcomeDescription(FloatRange qualityRange, string qualityNumber, PsychicRitualRoleAssignments assignments)
+        {
+            string desc = outcomeDescription.Formatted();
+            Thing inputThing = ProviderBox?.slotGroup.HeldThings.FirstOrDefault() ?? null;
+            if (inputThing != null)
+            {
+                float mult = MultFromQuality(qualityRange.min) * MultFromResearch();
+                int inputAmount = InputAmount(inputThing);
+                int outputAmount = Mathf.FloorToInt(inputAmount * mult);
+                desc += "AnomaliesExpected.ProviderScripture.Ritual.OutputDesc".Translate($"{inputThing.def.label} x{inputAmount}", MultFromQuality(qualityRange.min).ToStringPercent(), MultFromResearch().ToStringPercent(), mult.ToStringPercent(), $"{inputThing.def.label} x{outputAmount}");
+            }
+            return desc;
+        }
+
+        public float MultFromQuality(float Quality)
+        {
+            return multFromQuality.FirstOrDefault((Vector2 v) => Quality <= v.x).y;
+        }
+
+        public float MultFromResearch()
+        {
+            float mult = 1f;
+            if (researchProjectDef != null)
+            {
+                mult += researchProjectDef.ProgressReal / researchProjectDef.knowledgeCost * maxResearchMult;
+            }
+            return mult;
+        }
+
+        public int InputAmount(Thing inputThing)
+        {
+            return Mathf.Min(Mathf.FloorToInt(MaxWealth / inputThing.def.BaseMarketValue), inputThing.stackCount); ;
+        }
+
+        private IReadOnlyDictionary<PsychicRitualRoleDef, List<IntVec3>> GenerateRolePositions(PsychicRitualRoleAssignments assignments)
+        {
+            tmpParticipants.ClearAndPoolValueLists();
+            foreach (PsychicRitualRoleDef role in Roles)
+            {
+                tmpParticipants[role] = SimplePool<List<IntVec3>>.Get();
+            }
+            int num = assignments.RoleAssignedCount(ChanterRole) + assignments.RoleAssignedCount(InvokerRole);
+            int num2 = 0;
+            foreach (Pawn item in assignments.AssignedPawns(InvokerRole))
+            {
+                _ = item;
+                int num3 = 0;
+                IntVec3 cell;
+                do
+                {
+                    cell = assignments.Target.Cell;
+                    cell += IntVec3.FromPolar(360f * (float)num2++ / (float)num, invocationCircleRadius);
+                }
+                while (!cell.Walkable(assignments.Target.Map) && num3++ <= 10);
+                if (num3 >= 10)
+                {
+                    cell = assignments.Target.Cell;
+                }
+                tmpParticipants[InvokerRole].Add(cell);
+            }
+            foreach (Pawn item2 in assignments.AssignedPawns(ChanterRole))
+            {
+                _ = item2;
+                IntVec3 cell2 = assignments.Target.Cell;
+                cell2 += IntVec3.FromPolar(360f * (float)num2++ / (float)num, invocationCircleRadius);
+                tmpParticipants[ChanterRole].Add(cell2);
+            }
+            foreach (Pawn item3 in assignments.AssignedPawns(TargetRole))
+            {
+                _ = item3;
+                tmpParticipants[TargetRole].Add(assignments.Target.Cell);
+            }
+            if (DefenderRole != null)
+            {
+                num2 = 0;
+                int num4 = assignments.RoleAssignedCount(DefenderRole);
+                bool playerRitual = assignments.AllAssignedPawns.Any((Pawn x) => x.Faction == Faction.OfPlayer);
+                foreach (Pawn item4 in assignments.AssignedPawns(DefenderRole))
+                {
+                    _ = item4;
+                    IntVec3 cell3 = assignments.Target.Cell;
+                    cell3 += IntVec3.FromPolar(360f * (float)num2++ / (float)num4, invocationCircleRadius + 5f);
+                    cell3 = GetBestStandableRolePosition(playerRitual, cell3, assignments.Target.Cell, assignments.Target.Map);
+                    tmpParticipants[DefenderRole].Add(cell3);
+                }
+            }
+            return tmpParticipants;
+        }
+
     }
 }
--- Source/Anomalies Expected/PsychicRitual/PsychicRitualToil_AEAskingProvider.cs
+++ Source/Anomalies Expected/PsychicRitual/PsychicRitualToil_AEAskingProvider.cs
@@ -1,48 +1,69 @@
-﻿using Mono.Unix.Native;
-using RimWorld;
+﻿using RimWorld;
+using System.Collections.Generic;
 using System.Linq;
+using UnityEngine;
 using Verse;
 using Verse.AI.Group;
 
 namespace AnomaliesExpected
 {
-    public class PsychicRitualToil_AEAskingProvider : PsychicRitualToil
+    public class PsychicRitualToil_AEAskingProvider : PsychicRitualToil_InvokeHorax
     {
-        private PsychicRitualRoleDef invokerRole;
         public Building_Storage ProviderBox;
+        private ThingDef inputThingDef;
+        private int inputAmount;
 
-        protected PsychicRitualToil_AEAskingProvider()
+        protected PsychicRitualToil_AEAskingProvider() : base()
         {
         }
 
-        public PsychicRitualToil_AEAskingProvider(PsychicRitualRoleDef invokerRole)
+        public PsychicRitualToil_AEAskingProvider(PsychicRitualRoleDef invokerRole, IEnumerable<IntVec3> invokerPositions, PsychicRitualRoleDef targetRole, IEnumerable<IntVec3> targetPositions, PsychicRitualRoleDef chanterRole, IEnumerable<IntVec3> chanterPositions, PsychicRitualRoleDef defenderRole, IEnumerable<IntVec3> defenderPositions, IngredientCount requiredOffering) :
+            base(invokerRole, invokerPositions, targetRole, targetPositions, chanterRole, chanterPositions, defenderRole, defenderPositions, requiredOffering)
         {
-            this.invokerRole = invokerRole;
         }
 
         public override void Start(PsychicRitual psychicRitual, PsychicRitualGraph parent)
         {
             base.Start(psychicRitual, parent);
-            Pawn pawn = psychicRitual.assignments.FirstAssignedPawn(invokerRole);
-            psychicRitual.ReleaseAllPawnsAndBuildings();
-            if (pawn != null)
-            {
-                ApplyOutcome(psychicRitual, pawn);
-            }
+            PsychicRitualDef_AEAskingProvider psychicRitualDef = psychicRitual.def as PsychicRitualDef_AEAskingProvider;
+            Thing inputThing = ProviderBox.slotGroup.HeldThings.FirstOrDefault();
+            inputThingDef = inputThing.def;
+            inputAmount = psychicRitualDef.InputAmount(inputThing);
+            inputThing.SplitOff(inputAmount).Destroy();
         }
 
-        private void ApplyOutcome(PsychicRitual psychicRitual, Pawn invoker)
+        public override void End(PsychicRitual psychicRitual, PsychicRitualGraph parent, bool success)
         {
-            Thing inputThing = ProviderBox.slotGroup.HeldThings.FirstOrDefault();
-            Thing resource = ThingMaker.MakeThing(inputThing.def);
-            resource.stackCount = resource.def.stackLimit;
-            GenPlace.TryPlaceThing(resource, psychicRitual.assignments.Target.Cell, psychicRitual.assignments.Target.Map, ThingPlaceMode.Near, null);
+            base.End(psychicRitual, parent, success);
+            if (success)
+            {
+                IntVec3 position = psychicRitual.assignments.Target.Cell;
+                Map map = psychicRitual.assignments.Target.Map;
+                PsychicRitualDef_AEAskingProvider psychicRitualDef = psychicRitual.def as PsychicRitualDef_AEAskingProvider;
+                int outputAmount = Mathf.FloorToInt(inputAmount * psychicRitualDef.MultFromResearch() * psychicRitualDef.MultFromQuality(psychicRitual.PowerPercent));
+                Find.ResearchManager.ApplyKnowledge(psychicRitualDef.researchProjectDef, -Find.ResearchManager.GetKnowledge(psychicRitualDef.researchProjectDef), out _);
+                while (outputAmount > inputThingDef.stackLimit)
+                {
+                    Thing thing = ThingMaker.MakeThing(inputThingDef);
+                    thing.stackCount = inputThingDef.stackLimit;
+                    GenPlace.TryPlaceThing(thing, position, map, ThingPlaceMode.Near, null);
+                    outputAmount -= inputThingDef.stackLimit;
+                }
+                if (outputAmount > 0)
+                {
+                    Thing thing2 = ThingMaker.MakeThing(inputThingDef);
+                    thing2.stackCount = outputAmount;
+                    GenPlace.TryPlaceThing(thing2, position, map, ThingPlaceMode.Near, null);
+                }
+            }
         }
 
         public override void ExposeData()
         {
             base.ExposeData();
-            Scribe_Defs.Look(ref invokerRole, "invokerRole");
+            Scribe_References.Look(ref ProviderBox, "ProviderBox");
+            Scribe_Values.Look(ref inputThingDef, "inputThingDef");
+            Scribe_Values.Look(ref inputAmount, "inputValue", 0);
         }
     }
 }
