42c7711094dd5eb211a148547eb350e1b8823c6d
MrHydralisk <113328125+MrHydralisk@users.noreply.github.com>
2024-12-31 04:42:06 +0300

    Added full SnowMan to CTree with weapon

---
+++ Source/Anomalies Expected/Comp/Comp_EntityWithWeapon.cs
@@ -0,0 +1,87 @@
+﻿using RimWorld;
+using System.Collections.Generic;
+using System.Linq;
+using Verse;
+using Verse.AI.Group;
+
+namespace AnomaliesExpected
+{
+    public class Comp_EntityWithWeapon : ThingComp
+    {
+        public ThingDef weaponDef;
+        public Pawn parentPawn => parent as Pawn;
+
+        public override void Notify_BecameVisible()
+        {
+            base.Notify_BecameVisible();
+            CheckIfCombatReady();
+        }
+
+        public override void PostSpawnSetup(bool respawningAfterLoad)
+        {
+            base.PostSpawnSetup(respawningAfterLoad);
+            TryFindWeaponDef();
+            TryGiveWeapon();
+        }
+
+        public override void CompTick()
+        {
+            base.CompTick();
+            if (parentPawn.IsHashIntervalTick(500))
+            {
+                CheckIfCombatReady();
+            }
+        }
+
+        public void CheckIfCombatReady()
+        {
+            if (!parentPawn.DeadOrDowned && parent.Spawned && parent.Faction != Faction.OfPlayer)
+            {
+                TryFindWeaponDef();
+                TryGiveWeapon();
+                TryGiveAssaultJob();
+            }
+        }
+
+        public void TryFindWeaponDef()
+        {
+            if (weaponDef == null)
+            {
+                weaponDef = DefDatabase<ThingDef>.AllDefsListForReading.Where((ThingDef t) => t.IsWeapon && !t.weaponTags.NullOrEmpty() && t.weaponTags.Any((string s) => parentPawn.kindDef.weaponTags.Contains(s))).RandomElement();
+            }
+        }
+
+        public void TryGiveWeapon()
+        {
+            List<string> weaponTags = parentPawn.kindDef.weaponTags;
+            if (weaponTags.NullOrEmpty())
+            {
+                return;
+            }
+            List<ThingWithComps> allEquipmentListForReading = parentPawn.equipment.AllEquipmentListForReading;
+            for (int i = 0; i < allEquipmentListForReading.Count; i++)
+            {
+                if (allEquipmentListForReading[i].def.weaponTags.Any((string t) => weaponTags.Contains(t)))
+                {
+                    return;
+                }
+            }
+            PawnWeaponGenerator.TryGenerateWeaponFor(parentPawn, default(PawnGenerationRequest));
+            parentPawn.equipment.AddEquipment(ThingMaker.MakeThing(weaponDef) as ThingWithComps);
+        }
+
+        public void TryGiveAssaultJob()
+        {
+            if (parentPawn.GetLord() == null)
+            {
+                LordMaker.MakeNewLord(parent.Faction, new LordJob_AssaultColony(), parent.MapHeld, new List<Pawn> { parentPawn });
+            }
+        }
+
+        public override void PostExposeData()
+        {
+            Scribe_Defs.Look(ref weaponDef, "weaponDef");
+            base.PostExposeData();
+        }
+    }
+}
---
+++ Source/Anomalies Expected/GenStep/GenStep_ChristmasTreeSnowMan.cs
@@ -0,0 +1,48 @@
+﻿using RimWorld;
+using Verse;
+
+namespace AnomaliesExpected
+{
+    public class GenStep_ChristmasTreeSnowMan : GenStep
+    {
+        private int decoSnowManCount = 15;
+        private PawnKindCount pawnKindCount;
+
+        public override int SeedPart => 26098423;
+
+        public override void Generate(Map map, GenStepParams parms)
+        {
+            for (int i = 0; i < decoSnowManCount; i++)
+            {
+                if (!CellFinder.TryFindRandomCell(map, (IntVec3 c) => Validator(c, map), out IntVec3 result))
+                {
+                    continue;
+                }
+                GenSpawn.Spawn(ThingMaker.MakeThing(ThingDefOf.Snowman), result, map);
+            }
+            for (int i = 0; i < pawnKindCount.count; i++)
+            {
+                if (!CellFinder.TryFindRandomCell(map, (IntVec3 c) => Validator(c, map), out IntVec3 result))
+                {
+                    continue;
+                }
+                Pawn newThing = PawnGenerator.GeneratePawn(pawnKindCount.kindDef, Faction.OfEntities);
+                GenSpawn.Spawn(newThing, result, map);
+            }
+        }
+
+        private bool Validator(IntVec3 c, Map map)
+        {
+            if (!c.Standable(map))
+            {
+                return false;
+            }
+            if (c.DistanceToEdge(map) <= 2)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
+
