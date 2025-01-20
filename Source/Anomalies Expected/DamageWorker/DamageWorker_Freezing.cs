e4005c06db71d0767aeb1a0f0deb0830a7bba24d
MrHydralisk <113328125+MrHydralisk@users.noreply.github.com>
2025-01-21 00:19:27 +0300

    Freezing dmg scale of ComfyTempMin of Pawn instead of armor category SnowMan decrease combatPower Moved cs files to folders

---
+++ Source/Anomalies Expected/DamageWorker/DamageWorker_Freezing.cs
@@ -0,0 +1,19 @@
+﻿using RimWorld;
+using Verse;
+
+namespace AnomaliesExpected
+{
+    public class DamageWorker_Freezing : DamageWorker_Frostbite
+    {
+        public override DamageResult Apply(DamageInfo dinfo, Thing thing)
+        {
+            AE_DamageDefExtension damageDefExtension = dinfo.Def.GetModExtension<AE_DamageDefExtension>();
+            if (thing is Pawn pawn && damageDefExtension != null)
+            {
+                float statValue = pawn.GetStatValue(StatDefOf.ComfyTemperatureMin);
+                dinfo.SetAmount(dinfo.Amount * damageDefExtension.DamageMultiplierCurve.Evaluate(statValue));
+            }
+            return base.Apply(dinfo, thing);
+        }
+    }
+}
---
+++ Source/Anomalies Expected/DefModExtension/AE_DamageDefExtension.cs
@@ -0,0 +1,9 @@
+﻿using Verse;
+
+namespace AnomaliesExpected
+{
+    public class AE_DamageDefExtension : DefModExtension
+    {
+        public SimpleCurve DamageMultiplierCurve = new SimpleCurve();
+    }
+}
