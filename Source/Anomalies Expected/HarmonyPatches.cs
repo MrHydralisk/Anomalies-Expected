da410d30093b800a12fb8dcfd1a4d6315cd94214
MrHydralisk <113328125+MrHydralisk@users.noreply.github.com>
2025-01-31 06:05:57 +0300

    SnowGolem body added Fixed SnowMan right leg Added patch to count new entity flesh

--- Source/Anomalies Expected/HarmonyPatches.cs
+++ Source/Anomalies Expected/HarmonyPatches.cs
@@ -15,6 +15,7 @@ namespace AnomaliesExpected
             Harmony val = new Harmony("rimworld.mrhydralisk.AnomaliesExpectedPatch");
 
             val.Patch(AccessTools.Property(typeof(ResearchProjectDef), "IsHidden").GetGetMethod(), prefix: new HarmonyMethod(patchType, "RPD_IsHidden_Prefix"));
+            val.Patch(AccessTools.Property(typeof(RaceProperties), "IsAnomalyEntity").GetGetMethod(), postfix: new HarmonyMethod(patchType, "RP_IsAnomalyEntity_Postfix"));
         }
 
         public static bool RPD_IsHidden_Prefix(ref bool __result, ResearchProjectDef __instance)
@@ -25,6 +26,11 @@ namespace AnomaliesExpected
                 return false;
             }
             return true;
+        }
+
+        public static void RP_IsAnomalyEntity_Postfix(ref bool __result, RaceProperties __instance)
+        {
+            __result = __result || __instance.FleshType == FleshTypeDefOfLocal.AE_EntitySnowArmy;
         }
     }
 }
