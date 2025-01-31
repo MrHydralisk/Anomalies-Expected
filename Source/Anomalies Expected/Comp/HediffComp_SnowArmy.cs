68f1edcdc0a9eda809b840fc8f9b911d8254e349
MrHydralisk <113328125+MrHydralisk@users.noreply.github.com>
2025-01-31 07:50:51 +0300

    SnowArmy hediff for increased damage when burning and increased regeneration when cold Added butcher product to SnowArmy

---
+++ Source/Anomalies Expected/Comp/HediffComp_SnowArmy.cs
@@ -0,0 +1,35 @@
+﻿using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace AnomaliesExpected
+{
+    public class HediffComp_SnowArmy : HediffComp
+    {
+        public override void CompPostTick(ref float severityAdjustment)
+        {
+            base.CompPostTick(ref severityAdjustment);
+            if (Pawn.IsHashIntervalTick(250))
+            {
+                float temp = -Pawn.AmbientTemperature * Pawn.BodySize;
+                if (temp < 0)
+                {
+                    parent.Severity = 1;
+                }
+                else
+                {
+                    parent.Severity = Mathf.Max(2, temp);
+                }
+            }
+        }
+
+        public override void Notify_PawnPostApplyDamage(DamageInfo dinfo, float totalDamageDealt)
+        {
+            base.Notify_PawnPostApplyDamage(dinfo, totalDamageDealt);
+            if (dinfo.Def != DamageDefOf.Burn && Pawn.IsBurning())
+            {
+                dinfo.SetAmount(2f * dinfo.Amount);
+            }
+        }
+    }
+}
