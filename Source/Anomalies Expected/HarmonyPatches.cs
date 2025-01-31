2a0b99f6d54f62c80e5d9f81e4b4920418d31fe0
MrHydralisk <113328125+MrHydralisk@users.noreply.github.com>
2025-01-31 17:23:34 +0300

    Custom Faction for SnowArmy SnowGolem now ignores fire

--- Source/Anomalies Expected/HarmonyPatches.cs
+++ Source/Anomalies Expected/HarmonyPatches.cs
@@ -1,4 +1,5 @@
 ﻿using HarmonyLib;
+using RimWorld;
 using System;
 using Verse;
 
@@ -16,6 +17,7 @@ namespace AnomaliesExpected
 
             val.Patch(AccessTools.Property(typeof(ResearchProjectDef), "IsHidden").GetGetMethod(), prefix: new HarmonyMethod(patchType, "RPD_IsHidden_Prefix"));
             val.Patch(AccessTools.Property(typeof(RaceProperties), "IsAnomalyEntity").GetGetMethod(), postfix: new HarmonyMethod(patchType, "RP_IsAnomalyEntity_Postfix"));
+            val.Patch(AccessTools.Method(typeof(BackCompatibility), "FactionManagerPostLoadInit"), postfix: new HarmonyMethod(patchType, "BC_FactionManagerPostLoadInit_Postfix"));
         }
 
         public static bool RPD_IsHidden_Prefix(ref bool __result, ResearchProjectDef __instance)
@@ -31,6 +33,22 @@ namespace AnomaliesExpected
         public static void RP_IsAnomalyEntity_Postfix(ref bool __result, RaceProperties __instance)
         {
             __result = __result || __instance.FleshType == FleshTypeDefOfLocal.AE_EntitySnowArmy;
+        }
+
+        public static void BC_FactionManagerPostLoadInit_Postfix()
+        {
+            Faction SnowArmy = Find.FactionManager.FirstFactionOfDef(FactionDefOfLocal.AE_SnowArmy);
+            if (SnowArmy == null)
+            {
+                SnowArmy = FactionGenerator.NewGeneratedFaction(new FactionGeneratorParms(FactionDefOfLocal.AE_SnowArmy));
+                Find.FactionManager.Add(SnowArmy);
+            }
+            FactionRelation factionRelationAB = SnowArmy.RelationWith(Find.FactionManager.OfMechanoids);
+            factionRelationAB.baseGoodwill = 0;
+            factionRelationAB.kind = FactionRelationKind.Neutral;
+            FactionRelation factionRelationBA = Find.FactionManager.OfMechanoids.RelationWith(SnowArmy);
+            factionRelationBA.baseGoodwill = 0;
+            factionRelationBA.kind = FactionRelationKind.Neutral;
         }
     }
 }
