6c26a18f07565cd7ba6d8469ca3e453daf3c29e3
MrHydralisk <113328125+MrHydralisk@users.noreply.github.com>
2025-02-03 00:31:57 +0300

    SnowGolem can extinguish fire adn decrease temp

--- Source/Anomalies Expected/Comp/HediffComp/HediffComp_SnowBlockCompressor.cs
+++ Source/Anomalies Expected/Comp/HediffComp/HediffComp_SnowBlockCompressor.cs
@@ -1,5 +1,6 @@
 ﻿using RimWorld;
-using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
 using Verse;
 using Verse.Sound;
 
@@ -7,33 +8,30 @@ namespace AnomaliesExpected
 {
     public class HediffComp_SnowBlockCompressor : HediffComp
     {
-        private const float Radius = 6.9f;
-        private Color color = new Color(0.31f, 0.69f, 0.835f, 1);
+        public HediffCompProperties_SnowBlockCompressor Props => (HediffCompProperties_SnowBlockCompressor)props;
+
+        private int TickNextExtinguish;
 
         public override void CompPostTick(ref float severityAdjustment)
         {
             base.CompPostTick(ref severityAdjustment);
             if (Pawn.IsHashIntervalTick(250))
             {
-                if (TryExtinguish())
-                {
-                    SoundInfo soundInfo = new TargetInfo(Pawn.Position, Pawn.Map);
-                    soundInfo.volumeFactor *= 0.2f;
-                    soundInfo.pitchFactor *= 0.5f;
-                    SoundDefOfLocal.Explosion_Stun.PlayOneShot(soundInfo);
-                    foreach (IntVec3 cell in GenRadial.RadialCellsAround(Pawn.Position, Radius, true))
-                    {
-                        FleckMaker.ThrowExplosionCell(cell, Pawn.Map, FleckDefOf.AirPuff, color);
-                    }
-                    //FleckMaker.WaterSplash(Pawn.Position.ToVector3Shifted(), Pawn.Map, Radius * 6f, 20f);
-                }
+                TryExtinguish();
                 TryPushCold();
             }
         }
-        public bool TryExtinguish()
+
+        public void TryExtinguish()
         {
+            if (Find.TickManager.TicksGame < TickNextExtinguish)
+            {
+                return;
+            }
             bool succeed = false;
-            foreach (Thing thing in GenRadial.RadialDistinctThingsAround(Pawn.Position, Pawn.Map, Radius, true))
+            List<IntVec3> cells = GenRadial.RadialCellsAround(Pawn.Position, Props.radius, true).ToList();
+            List<Thing> things = cells.SelectMany((IntVec3 iv3) => Pawn.Map.thingGrid.ThingsListAtFast(iv3)).ToList();
+            foreach (Thing thing in things)
             {
                 if (thing is Fire fire && !fire.DestroyedOrNull())
                 {
@@ -41,17 +39,26 @@ namespace AnomaliesExpected
                     succeed = true;
                 }
             }
-            return succeed;
+            if (succeed)
+            {
+                TickNextExtinguish = Find.TickManager.TicksGame + Props.ticksBetweenExtinguish;
+                SoundInfo soundInfo = new TargetInfo(Pawn.Position, Pawn.Map);
+                soundInfo.volumeFactor *= 0.2f;
+                soundInfo.pitchFactor *= 0.5f;
+                Props.soundDef.PlayOneShot(soundInfo);
+                foreach (IntVec3 cell in cells)
+                {
+                    FleckMaker.ThrowExplosionCell(cell, Pawn.Map, Props.fleckDef, Props.color);
+                }
+            }
         }
 
-        public bool TryPushCold()
+        public void TryPushCold()
         {
             if (Pawn.Spawned && Pawn.AmbientTemperature > -273f)
             {
-                GenTemperature.PushHeat(Pawn, Pawn.BodySize * -50f);
-                return true;
+                GenTemperature.PushHeat(Pawn, Pawn.BodySize * Props.heatPushedPerBodySize);
             }
-            return false;
         }
 
         public override void Notify_PawnPostApplyDamage(DamageInfo dinfo, float totalDamageDealt)
@@ -61,6 +68,11 @@ namespace AnomaliesExpected
             {
                 dinfo.SetAmount(2f * dinfo.Amount);
             }
+        }
+
+        public override void CompExposeData()
+        {
+            Scribe_Values.Look(ref TickNextExtinguish, "TickNextEmerge", -1);
         }
     }
 }
---
+++ Source/Anomalies Expected/CompProperties/HediffCompProperties/HediffCompProperties_SnowBlockCompressor.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using Verse;
+
+namespace AnomaliesExpected
+{
+    public class HediffCompProperties_SnowBlockCompressor : HediffCompProperties_LetterOnDeath
+    {
+        public float radius = 6.9f;
+        public int ticksBetweenExtinguish = 2500;
+        public float heatPushedPerBodySize = -10f;
+        public Color color = new Color(0.31f, 0.69f, 0.835f, 0.5f);
+        public FleckDef fleckDef;
+        public SoundDef soundDef;
+
+        public HediffCompProperties_SnowBlockCompressor()
+        {
+            compClass = typeof(HediffComp_SnowBlockCompressor);
+        }
+    }
+}
