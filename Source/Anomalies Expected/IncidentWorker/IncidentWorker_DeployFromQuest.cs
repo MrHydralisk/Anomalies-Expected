bb64752a4dcb3a903605bd4029a993b1f6c3dfe3
MrHydralisk <113328125+MrHydralisk@users.noreply.github.com>
2025-05-02 00:23:10 +0300

    Added Mysterious Cargo for MeatGrinder, BakingPies and BeamTarget

---
+++ Source/Anomalies Expected/IncidentWorker/IncidentWorker_DeployFromQuest.cs
@@ -0,0 +1,44 @@
+﻿using RimWorld;
+using Verse;
+
+namespace AnomaliesExpected
+{
+    public class IncidentWorker_DeployFromQuest : IncidentWorker_AE
+    {
+        protected override bool CanFireNowSub(IncidentParms parms)
+        {
+            if (!base.CanFireNowSub(parms))
+            {
+                return false;
+            }
+            if (def.questScriptDef != null)
+            {
+                if (!def.questScriptDef.CanRun(parms.points))
+                {
+                    return false;
+                }
+            }
+            else if (parms.questScriptDef != null && !parms.questScriptDef.CanRun(parms.points))
+            {
+                return false;
+            }
+            return PawnsFinder.AllMapsCaravansAndTravelingTransportPods_Alive_FreeColonists_NoSuspended.Any();
+        }
+
+        protected override bool TryExecuteWorker(IncidentParms parms)
+        {
+            QuestScriptDef questScriptDef = def.questScriptDef ?? parms.questScriptDef ?? NaturalRandomQuestChooser.ChooseNaturalRandomQuest(parms.points, parms.target);
+            if (questScriptDef == null)
+            {
+                return false;
+            }
+            parms.questScriptDef = questScriptDef;
+            Quest quest = QuestUtility.GenerateQuestAndMakeAvailable(questScriptDef, parms.points);
+            if (!quest.hidden && questScriptDef.sendAvailableLetter)
+            {
+                QuestUtility.SendLetterQuestAvailable(quest);
+            }
+            return true;
+        }
+    }
+}
---
+++ Source/Anomalies Expected/QuestNode_Root_AEMysteriousCargo.cs
@@ -0,0 +1,15 @@
+﻿using RimWorld.QuestGen;
+using Verse;
+
+namespace AnomaliesExpected
+{
+    public class QuestNode_Root_AEMysteriousCargo : QuestNode_Root_MysteriousCargo
+    {
+        public ThingDef DeployableObjectDef;
+
+        protected override Thing GenerateThing(Pawn _)
+        {
+            return ThingMaker.MakeThing(DeployableObjectDef);
+        }
+    }
+}
