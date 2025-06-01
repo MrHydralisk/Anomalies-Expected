b485ca8ddf5666a1f609fa14f872cfef04e96ed2
MrHydralisk <113328125+MrHydralisk@users.noreply.github.com>
2025-06-01 04:52:43 +0300

    BCake added study notes

--- Source/Anomalies Expected/Comp/CompInteractable/Comp_BirthdayCake.cs
+++ Source/Anomalies Expected/Comp/CompInteractable/Comp_BirthdayCake.cs
@@ -12,7 +12,6 @@ namespace AnomaliesExpected
     {
         public new CompProperties_BirthdayCake Props => (CompProperties_BirthdayCake)props;
 
-
         protected CompAEStudyUnlocks StudyUnlocks => studyUnlocksCached ?? (studyUnlocksCached = parent.TryGetComp<CompAEStudyUnlocks>());
         private CompAEStudyUnlocks studyUnlocksCached;
 
@@ -45,69 +44,11 @@ namespace AnomaliesExpected
                 }
                 yield return gizmo;
             }
-            if (DebugSettings.ShowDevGizmos)
-            {
-                //yield return new Command_Action
-                //{
-                //    action = delegate
-                //    {
-                //        List<FloatMenuOption> floatMenuOptions = new List<FloatMenuOption>();
-                //        floatMenuOptions.Add(new FloatMenuOption("Increase by 3d", delegate
-                //        {
-                //            TickSpawn += 180000;
-                //        }));
-                //        floatMenuOptions.Add(new FloatMenuOption("Increase by 1d", delegate
-                //        {
-                //            TickSpawn += 60000;
-                //        }));
-                //        floatMenuOptions.Add(new FloatMenuOption("Increase by 1h", delegate
-                //        {
-                //            TickSpawn += 2500;
-                //        }));
-                //        floatMenuOptions.Add(new FloatMenuOption("Decrease by 1h", delegate
-                //        {
-                //            TickSpawn -= 2500;
-                //        }));
-                //        floatMenuOptions.Add(new FloatMenuOption("Decrease by 1d", delegate
-                //        {
-                //            TickSpawn -= 60000;
-                //        }));
-                //        floatMenuOptions.Add(new FloatMenuOption("Decrease by 3d", delegate
-                //        {
-                //            TickSpawn -= 180000;
-                //        }));
-                //        Find.WindowStack.Add(new FloatMenu(floatMenuOptions));
-                //    },
-                //    defaultLabel = "Dev: Change Spawn",
-                //    defaultDesc = $"Change timer till spawn piece of pie: {(TickSpawn - Find.TickManager.TicksGame).ToStringTicksToPeriodVerbose()}"
-                //};
-            }
         }
 
         protected override void OnInteracted(Pawn caster)
         {
             CelebrateBirthday(caster);
         }
-
-        public override void PostExposeData()
-        {
-            base.PostExposeData();
-        }
-
-
-        //public override string CompInspectStringExtra()
-        //{
-        //    List<string> inspectStrings = new List<string>();
-        //    int study = StudyUnlocks?.NextIndex ?? 4;
-        //    if (study > 1)
-        //    {
-        //        inspectStrings.Add("AnomaliesExpected.BakingPies.Amount".Translate(CalculatePiePieces(false)).RawText);
-        //        if (study > 2)
-        //        {
-        //            inspectStrings.Add("AnomaliesExpected.BakingPies.Time".Translate((TickSpawn - Find.TickManager.TicksGame).ToStringTicksToPeriodVerbose()).RawText);
-        //        }
-        //    }
-        //    return String.Join("\n", inspectStrings);
-        //}
     }
 }
