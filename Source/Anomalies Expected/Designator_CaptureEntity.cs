cf064441dc9a6009950c6e383080c380cf42b4b7
MrHydralisk <113328125+MrHydralisk@users.noreply.github.com>
2025-06-07 07:13:09 +0300

    Designator CaptureEntity now sends entity to diff platforms

--- Source/Anomalies Expected/Designator_CaptureEntity.cs
+++ Source/Anomalies Expected/Designator_CaptureEntity.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using Verse;
+using Verse.AI;
 
 namespace AnomaliesExpected
 {
 
     public class Designator_CaptureEntity : Designator
     {
+        private List<Building_HoldingPlatform> HoldingPlatforms = new List<Building_HoldingPlatform>();
+
         public override int DraggableDimensions => 2;
 
         protected override DesignationDef Designation => DesignationDefOfLocal.AE_CaptureEntity;
@@ -41,6 +44,40 @@ namespace AnomaliesExpected
             return true;
         }
 
+        public override AcceptanceReport CanDesignateThing(Thing t)
+        {
+            CompHoldingPlatformTarget compHoldingPlatformTarget = t.TryGetComp<CompHoldingPlatformTarget>();
+            return compHoldingPlatformTarget != null && compHoldingPlatformTarget.StudiedAtHoldingPlatform && !compHoldingPlatformTarget.CurrentlyHeldOnPlatform && compHoldingPlatformTarget.CanBeCaptured && t.Spawned;
+        }
+
+        public override void DesignateMultiCell(IEnumerable<IntVec3> cells)
+        {
+            if (TutorSystem.TutorialMode && !TutorSystem.AllowAction(new EventPack(TutorTagDesignate, cells)))
+            {
+                return;
+            }
+            bool somethingSucceeded = false;
+            bool flag = false;
+            HoldingPlatforms = base.Map.listerBuildings.AllBuildingsColonistOfClass<Building_HoldingPlatform>().Where(b => !b.Occupied).ToList();
+            foreach (IntVec3 cell in cells)
+            {
+                if (CanDesignateCell(cell).Accepted)
+                {
+                    DesignateSingleCell(cell);
+                    somethingSucceeded = true;
+                    if (!flag)
+                    {
+                        flag = ShowWarningForCell(cell);
+                    }
+                }
+            }
+            Finalize(somethingSucceeded);
+            if (TutorSystem.TutorialMode)
+            {
+                TutorSystem.Notify_Event(new EventPack(TutorTagDesignate, cells));
+            }
+        }
+
         public override void DesignateSingleCell(IntVec3 loc)
         {
             foreach (Thing thing in CaptureEntitiesInCell(loc))
@@ -49,17 +86,25 @@ namespace AnomaliesExpected
             }
         }
 
-        public override AcceptanceReport CanDesignateThing(Thing t)
-        {
-            CompHoldingPlatformTarget compHoldingPlatformTarget = t.TryGetComp<CompHoldingPlatformTarget>();
-            return compHoldingPlatformTarget != null && compHoldingPlatformTarget.StudiedAtHoldingPlatform && !compHoldingPlatformTarget.CurrentlyHeldOnPlatform && compHoldingPlatformTarget.CanBeCaptured && t.Spawned;
-        }
-
         public override void DesignateThing(Thing t)
         {
+            if (HoldingPlatforms.NullOrEmpty())
+            {
+                return;
+            }
             base.Map.designationManager.RemoveAllDesignationsOn(t);
             base.Map.designationManager.AddDesignation(new Designation(t, Designation));
-            StudyUtility.TargetHoldingPlatformForEntity(null, t);
+            CompHoldingPlatformTarget compHoldingPlatformTarget = t.TryGetComp<CompHoldingPlatformTarget>();
+            Thing building = GenClosest.ClosestThing_Global_Reachable(t.Position, base.Map, HoldingPlatforms, PathEndMode.ClosestTouch, TraverseParms.For(TraverseMode.PassDoors), 9999f, null, delegate (Thing t)
+            {
+                CompEntityHolder CompEntityHolder = t.TryGetComp<CompEntityHolder>();
+                return (CompEntityHolder != null && CompEntityHolder.ContainmentStrength >= t.GetStatValue(StatDefOf.MinimumContainmentStrength)) ? (CompEntityHolder.ContainmentStrength / Mathf.Max(t.PositionHeld.DistanceTo(t.Position), 1f)) : 0f;
+            });
+            if (building != null)
+            {
+                compHoldingPlatformTarget.targetHolder = building;
+                HoldingPlatforms.Remove(building as Building_HoldingPlatform);
+            }
         }
 
         private IEnumerable<Thing> CaptureEntitiesInCell(IntVec3 c)
