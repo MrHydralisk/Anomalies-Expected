008de3ba1fa7fc19b54f95b67ee04278596593ad
MrHydralisk <113328125+MrHydralisk@users.noreply.github.com>
2025-06-09 23:11:59 +0300

    Added ability and trait for Grinder creep

---
+++ Source/Anomalies Expected/Comp/CompAbilityEffect/CompAbilityEffect_GrindBody.cs
@@ -0,0 +1,45 @@
+﻿using RimWorld;
+using System.Collections.Generic;
+using UnityEngine;
+using Verse;
+
+namespace AnomaliesExpected
+{
+    public class CompAbilityEffect_GrindBody : CompAbilityEffect
+    {
+        public new CompProperties_AbilityGrindBody Props => (CompProperties_AbilityGrindBody)props;
+
+        private Pawn Pawn => parent.pawn;
+
+        public override void Apply(LocalTargetInfo target, LocalTargetInfo dest)
+        {
+            base.Apply(target, dest);
+            if (target.Thing is Corpse corpse && corpse.InnerPawn != null)
+            {
+                IEnumerable<Thing> products = corpse.InnerPawn.ButcherProducts(Pawn, Props.butcherEfficiency);
+                if (corpse.InnerPawn.RaceProps.BloodDef != null)
+                {
+                    FilthMaker.TryMakeFilth(corpse.Position, corpse.Map, corpse.InnerPawn.RaceProps.BloodDef, corpse.InnerPawn.LabelIndefinite());
+                }
+                if (corpse.InnerPawn.RaceProps.Humanlike)
+                {
+                    Find.HistoryEventsManager.RecordEvent(new HistoryEvent(HistoryEventDefOf.ButcheredHuman, new SignalArgs(Pawn.Named(HistoryEventArgsNames.Doer), corpse.InnerPawn.Named(HistoryEventArgsNames.Victim))));
+                }
+                foreach (Thing product in products)
+                {
+                    GenPlace.TryPlaceThing(product, corpse.Position, corpse.Map, ThingPlaceMode.Near);
+                }
+                corpse.Destroy();
+            }
+        }
+
+        public override bool AICanTargetNow(LocalTargetInfo target)
+        {
+            if (Pawn.Faction != null && target.Thing is Corpse corpse && corpse.InnerPawn != null && corpse.Faction != Pawn.Faction)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ Source/Anomalies Expected/CompProperties/CompProperties_AbilityEffect/CompProperties_AbilityGrindBody.cs
@@ -0,0 +1,15 @@
+﻿using RimWorld;
+using Verse;
+
+namespace AnomaliesExpected
+{
+    public class CompProperties_AbilityGrindBody : CompProperties_AbilityEffect
+    {
+        public float butcherEfficiency = 2f;
+
+        public CompProperties_AbilityGrindBody()
+        {
+            compClass = typeof(CompAbilityEffect_GrindBody);
+        }
+    }
+}
