6f3d582b3c757e48901c89f56cb65f363749fac2
MrHydralisk <113328125+MrHydralisk@users.noreply.github.com>
2025-07-19 00:41:37 +0300

    DogTag spawn per tiles in Lake

--- Source/Anomalies Expected/GenStep/GenStep_BloodLakeFindDogTag.cs
+++ Source/Anomalies Expected/GenStep/GenStep_BloodLakeFindDogTag.cs
@@ -1,21 +1,28 @@
-﻿using RimWorld.Planet;
+﻿using RimWorld;
+using RimWorld.Planet;
 using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
 using Verse;
 
 namespace AnomaliesExpected
 {
     public class GenStep_BloodLakeFindDogTag : GenStep
     {
+        private int numDogTagsPerTiles = 625;
+
         public override int SeedPart => 12412314;
 
         public override void Generate(Map map, GenStepParams parms)
         {
             Map sourceMap = (map.Parent as PocketMapParent)?.sourceMap;
-            List<Pawn> colonists = sourceMap?.mapPawns?.FreeColonistsSpawned;
+            List<Pawn> colonists = sourceMap?.mapPawns?.FreeColonistsSpawned.ToList();
             if (!colonists.NullOrEmpty())
             {
-                foreach (Pawn pawn in colonists)
+                int num = Mathf.RoundToInt(map.Size.ToIntVec2.Area / (float)numDogTagsPerTiles);
+                for (int i = 0; i < num && !colonists.NullOrEmpty(); i++)
                 {
+                    Pawn pawn = colonists.RandomElement();
                     IntVec3 pos = IntVec3.Invalid;
                     CellFinder.TryFindRandomCell(map, (IntVec3 c) => Validator(c, map), out pos);
                     if (pos != IntVec3.Invalid)
@@ -23,6 +30,7 @@ namespace AnomaliesExpected
                         ThingWithComps thing = GenSpawn.Spawn(ThingMaker.MakeThing(ThingDefOfLocal.AE_DogTag), pos, map) as ThingWithComps;
                         Comp_DogTag dogTag = thing.GetComp<Comp_DogTag>();
                         dogTag.SetOwner(pawn);
+                        colonists.Remove(pawn);
                     }
                 }
             }
